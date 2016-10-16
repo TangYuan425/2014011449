4ea51eed0c6371860dd1d575003f9b687d022cba
TangYuan425 <577642093@qq.com>
2016-10-16 13:44:01 +0800

    Fragment

---
+++ Fragment/app/src/main/java/com/example/tangyuan/fragment/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.tangyuan.fragment;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.tangyuan.fragment.Words.WordsContent;
+
+import java.net.URI;
+
+public class MainActivity extends AppCompatActivity implements ItemFragment.OnListFragmentInteractionListener,
+        DetailFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onFragmentInteraction(String id) {
+        Log.v("test","onFragmentInteraction");
+        Bundle arguments =new Bundle();
+        arguments.putString("id",id);
+        DetailFragment fragment = new DetailFragment();
+        fragment.setArguments(arguments);
+        getFragmentManager().beginTransaction().replace(R.id.worddetail,fragment).commit();
+
+    }
+
+
+    public void onFragmentInteraction(URI uri) {
+
+    }
+
+    public void onListFragmentInteraction(WordsContent.WordItem item) {
+        Bundle arguments =new Bundle();
+        arguments.putString("id",item.id);
+        DetailFragment fragment=new DetailFragment();
+        fragment.setArguments(arguments);
+        getFragmentManager().beginTransaction().replace(R.id.worddetail,fragment).commit();
+    }
+
+}
